3f3c1a54fa54302b9410dc8055c3a5ba866888c0
Eduardo Vazquez <eduardo.vazquez@seattlecolleges.edu>
2022-08-08 10:31:22 -0700

    added other arithmetic operations

--- game-demo.js
+++ game-demo.js
@@ -23,11 +23,101 @@ function additionGame(randomNumber1, randomNumber2, number1, number2){
 
     if (computerSum > playerTotal){
         console.log("Computer wins!!!");
-    } else if (playerTotal < computerSum){
+    } else if (playerTotal > computerSum){
         console.log("Congrats!! You win!!");
     } else {
-        console.log("Error");
+        console.log("Its a tie!!");
     }
 }
 
-additionGame(randomNumber1, randomNumber2, 2, 5);
+function subtractionGame(randomNumber1, randomNumber2, number1, number2){
+    // computer portion 
+    console.log("Computer chose: " + randomNumber1);
+    console.log("Computer chose: " + randomNumber2);
+
+    let computerTotal = randomNumber1 - randomNumber2; // arithmetic calculation
+
+    console.log("Computer total is " + computerTotal);
+
+    console.log(" ");
+
+    // user portion
+    console.log("Enter a number: " + number1);
+    console.log("Enter another number: " + number2);
+
+    let playerTotal = number1 - number2; // arithmetic calculation
+    console.log("Total of numbers is: " + playerTotal);
+
+    console.log(" ");
+
+    if (computerTotal > playerTotal){
+        console.log("Computer wins!!!");
+    } else if (playerTotal > computerTotal){
+        console.log("Congrats!! You win!!");
+    } else {
+        console.log("Its a tie!!");
+    }
+}
+
+function multiplicationGame(randomNumber1, randomNumber2, number1, number2){
+    // computer portion 
+    console.log("Computer chose: " + randomNumber1);
+    console.log("Computer chose: " + randomNumber2);
+
+    let computerSum = randomNumber1 * randomNumber2; // arithmetic calculation
+
+    console.log("Computer total is " + computerSum);
+
+    console.log(" ");
+
+    // user portion
+    console.log("Enter a number: " + number1);
+    console.log("Enter another number: " + number2);
+
+    let playerTotal = number1 * number2; // arithmetic calculation
+    console.log("Sum of numbers is: " + playerTotal);
+
+    console.log(" ");
+
+    if (computerSum > playerTotal){
+        console.log("Computer wins!!!");
+    } else if (playerTotal > computerSum){
+        console.log("Congrats!! You win!!");
+    } else {
+        console.log("Its a tie!!");
+    }
+}
+
+function divisonGame(randomNumber1, randomNumber2, number1, number2){
+    // computer portion 
+    console.log("Computer chose: " + randomNumber1);
+    console.log("Computer chose: " + randomNumber2);
+
+    let computerSum = randomNumber1 / randomNumber2; // arithmetic calculation
+
+    console.log("Computer total is " + computerSum);
+
+    console.log(" ");
+
+    // user portion
+    console.log("Enter a number: " + number1);
+    console.log("Enter another number: " + number2);
+
+    let playerTotal = number1 / number2; // arithmetic calculation
+    console.log("Sum of numbers is: " + playerTotal);
+
+    console.log(" ");
+
+    if (computerSum > playerTotal){
+        console.log("Computer wins!!!");
+    } else if (playerTotal > computerSum){
+        console.log("Congrats!! You win!!");
+    } else {
+        console.log("Its a tie!!");
+    }
+}
+
+// additionGame(randomNumber1, randomNumber2, 3, 5);
+// subtractionGame(randomNumber1, randomNumber2, 10, 5);
+// multiplicationGame(randomNumber1, randomNumber2, 3, 2);
+// divisonGame(randomNumber1, randomNumber2, 10, 5);
